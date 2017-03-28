f83a9bd2b128269a7f3604ab8be74966d4a8486f
gogbajbobo <grigoriev.maxim@gmail.com>
2017-03-28 17:42:02 +0300

    scroll to sections

--- iptm.js
+++ iptm.js
@@ -2,6 +2,29 @@
  * Created by grimax on 28/03/2017.
  */
 
+$(document).ready(function() {
+
+    $(".navbar a, footer a[href='#iptmPage']").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+
+        }
+
+    });
+
+});
+
+
 $(window).scroll(function() {
 
     $(".slideanim").each(function() {
@@ -17,4 +40,3 @@ $(window).scroll(function() {
     });
 
 });
-
