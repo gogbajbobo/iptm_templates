1505ec49c0e2bf42f80c31eed6e8a6c6afecb845
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-20 11:46:53 +0300

    compensate smaller container padding

--- js/iptm.js
+++ js/iptm.js
@@ -23,7 +23,7 @@ function scrollToSection() {
             let hash = this.hash;
 
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 40
             }, 900, function() {
                 window.location.hash = hash;
             });
